a359e2b340325bd73441c6146a4929ed8b44f8d8
LuukFTF <lucasvdvegt@gmail.com>
2021-11-18 21:38:25 +0100

    first api made

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const PORT = 8080;
 
 app.listen(
     PORT,
-    () => console.log(`hij leeft http://localhost:${PORT}`)
+    () => console.log(`hij leeft op http://localhost:${PORT}`)
 )
+
+app.use( express.json() )
 
 app.get('/test', (req, res) => {
     res.status(200).send({
         foo: 'bruh'
     })
-})
+});
 
-// app.get('/test/:id', (req, res) => {
-//     const { id } = req.params;
-//     const { test } = req.body;
+app.post('/test/:id', (req, res) => {
+    const { id } = req.params;
+    const { test } = req.body;
 
-//     if (!test) {
-//         res.status(418).send({ message: 'We hebbe een testwaarde nodig'})
-//     }
+    if (!test) {
+        res.status(418).send({ message: `We hebbe een testwaarde nodig, nu ist ${id} & ${test}` })
+    }
 
-//     res.send({
-//         foo: `jo je id is ${id} en de testwaarde is `${test}`,
-//     });
-    
-// });
+    res.send({
+        foo: `jo je id is ${id} en de testwaarde is ${test}`,
+    });
+});
